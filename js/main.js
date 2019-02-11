c31c06b9b6e5f8e9b6e051734ba693b4bf9d46b1
luka-zelenovic <luka.zelenovic13@gmail.com>
2019-02-11 12:31:06 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
-
-window.onload = function () { 
-$(window).scrollTop(0);	
-}
+window.addEventListener("load", function() { window. scrollTo(0, 0); });
 
 arrayRed = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36];
 arrayBlack = [3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37];
